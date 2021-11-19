fc1c95c40f5204186da47998b764b7fe7de41d00
윤승준 <yid05011@naver.com>
2021-11-19 17:49:53 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/deploymentTargetDropDown.xml
    로그인 기능 회원가입 기능으로 시작
    회원가입 기능에 사용자 정보 추가
    회원가입 기능에 비밀번호 중복 아이디 중복 체크
    비밀번호 찾기 추가

--- app/src/main/java/com/example/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -21,7 +21,7 @@ public class RegisterActivity extends AppCompatActivity {
 
     private FirebaseAuth mFirebaseAuth;     //파이어베이스 인증
     private DatabaseReference nDatabaseRef; //실시간 데이터베이스
-    private EditText mEtEmail,mEtPwd,mEtName,mEtphoneNum;       //회원가입 입력필드
+    private EditText mEtEmail,mEtPwd,mEtPwd2,mEtName,mEtphoneNum;       //회원가입 입력필드
     private Button mBtnRegister;            //회원가입 버튼
 
 
@@ -35,47 +35,56 @@ public class RegisterActivity extends AppCompatActivity {
 
         mEtEmail=findViewById(R.id.et_email);
         mEtPwd=findViewById(R.id.et_pwd);
-        mBtnRegister=findViewById(R.id.btn_resetPwd);
+        mEtPwd2=findViewById(R.id.et_pwd2);
+        mBtnRegister=findViewById(R.id.btn_register);
         mEtName=findViewById(R.id.et_name);
         mEtphoneNum=findViewById(R.id.et_phoneNum);
+
+
+
 
         mBtnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // 회원가입 처리 시작
-                String strEmail =mEtEmail.getText().toString(); //문자열로 입력된 걸 가져옴
-                String strPwd=mEtPwd.getText().toString(); //변수에 할당
-                String strName=mEtName.getText().toString(); // 이름 할당
-                String strPhoneNum=mEtphoneNum.getText().toString(); //폰번호 할당
+                String strEmail = mEtEmail.getText().toString(); //문자열로 입력된 걸 가져옴
+                String strPwd = mEtPwd.getText().toString(); //변수에 할당
+                String strPwd2 = mEtPwd2.getText().toString(); //변수에 할당
+                String strName = mEtName.getText().toString(); // 이름 할당
+                String strPhoneNum = mEtphoneNum.getText().toString(); //폰번호 할당
+                if (!strPwd.equals(strPwd2)) {
+                    Toast.makeText(RegisterActivity.this, "비밀번호가 일치 하지 않습니다.", Toast.LENGTH_SHORT).show();
+                } else{
+                    //firebaseAuth 진행
+                    mFirebaseAuth.createUserWithEmailAndPassword(strEmail, strPwd).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) //인증처리가 완료될떄
+                        {
+                            if (task.isSuccessful()) {
+                                FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser(); //현재 유저 가져오기
+                                UserAccount account = new UserAccount();
+                                //데이터베이스에 삽입
+                                account.setIdToken(firebaseUser.getUid()); //고윳값
+                                account.setEmailId(firebaseUser.getEmail());
+                                account.setPassword(strPwd);
+                                account.setName(strName);
+                                account.setPhoneNum(strPhoneNum);
 
-                //firebaseAuth 진행
-                mFirebaseAuth.createUserWithEmailAndPassword(strEmail,strPwd).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) //인증처리가 완료될떄
-                    {
-                        if(task.isSuccessful()) {
-                            FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser(); //현재 유저 가져오기
-                            UserAccount account =new UserAccount();
-                            //데이터베이스에 삽입
-                            account.setIdToken(firebaseUser.getUid()); //고윳값
-                            account.setEmailId(firebaseUser.getEmail());
-                            account.setPassword(strPwd);
-                            account.setName(strName);
-                            account.setPhoneNum(strPhoneNum);
+                                //setValue :database에 insert(삽입)행위
+                                nDatabaseRef.child("UserAccount").child(firebaseUser.getUid()).setValue(account);
 
-                            //setValue :database에 insert(삽입)행위
-                            nDatabaseRef.child("UserAccount").child(firebaseUser.getUid()).setValue(account);
+                                Toast.makeText(RegisterActivity.this, "회원가입에 성공했습니다", Toast.LENGTH_SHORT).show();
+                                finish();
+                            } else {
+                                Toast.makeText(RegisterActivity.this, "이미 있는 이메일입니다.", Toast.LENGTH_SHORT).show();
 
-                            Toast.makeText (RegisterActivity.this,"회원가입에 성공했습니다",Toast.LENGTH_SHORT).show();
-                            finish();
-                        }else{
-                            Toast.makeText (RegisterActivity.this,"회원가입에 실패했습니다",Toast.LENGTH_SHORT).show();
-
+                            }
                         }
-                    }
 
 
-                });
+                    });
+            }
+
             }
         });
 
--- app/src/main/java/com/example/myapplication/resetPwdActivity.java
+++ app/src/main/java/com/example/myapplication/resetPwdActivity.java
@@ -1,6 +1,5 @@
 package com.example.myapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,11 +11,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class resetPwdActivity extends AppCompatActivity {
 
@@ -40,7 +36,7 @@ public class resetPwdActivity extends AppCompatActivity {
         mEtEmail=findViewById(R.id.et_email);
 
 
-        Button btn_restPwd=findViewById(R.id.btn_resetPwd);
+        Button btn_restPwd=findViewById(R.id.btn_register);
         btn_restPwd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
