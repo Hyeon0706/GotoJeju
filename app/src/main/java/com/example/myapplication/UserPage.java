19a7f1bf6584bb727717253e7842232973b2be3a
gramer12 <yid05011@naver.com>
2021-12-08 17:56:04 +0900

    마이페이지 제작1

--- app/src/main/java/com/example/myapplication/UserPage.java
+++ app/src/main/java/com/example/myapplication/UserPage.java
@@ -1,8 +1,16 @@
+/*
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.SeekBar;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -10,6 +18,39 @@ public class UserPage extends AppCompatActivity {
     private FirebaseAuth mFirebaseAuth;     //파이어베이스 인증
     private DatabaseReference nDatabaseRef; //실시간 데이터베이스
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+
+        if (user != null)
+        {
+
+            //String name = user.getDisplayName();
+            String email = user.getEmail();
+            //String phoneNum = user.getPhoneNumber();
+            // Name, email address, and profile photo Url
+            //TextView userName = findViewById(R.id.user_name);
+            TextView userEmail = findViewById(R.id.user_email);
+            //TextView userphoneNum=findViewById(R.id.user_phoneNum);
+
+
+
+
+            //userName.setText(name);
+            userEmail.setText(email);
+            //userphoneNum.setText(phoneNum);
+
+        }
 
     }
+
+
+
+
+
+
+
+
+}
+*/
